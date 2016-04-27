8039b3dba8f6ec56e1637de9d8531d476efea8ff
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-04-27 20:14:55 +0200

    Main gem entry point

---
+++ lib/unobtainium-faraday.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+#
+# unobtainium-faraday
+# https://github.com/jfinkhaeuser/unobtainium-faraday
+#
+# Copyright (c) 2016 Jens Finkhaeuser and other unobtainium-faraday contributors.
+# All rights reserved.
+#
+
+require 'unobtainium-faraday/driver'
